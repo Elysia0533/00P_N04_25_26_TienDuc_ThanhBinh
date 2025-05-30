034775ce9be06f6984f95565de1d49c37e5c7ab2
Le Thu Nguyen <nglthu1967@gmail.com>
2025-05-30 04:23:53 +0000

    song List Controller by @Controller

--- gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/SongListController.java
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/SongListController.java
@@ -1,17 +1,19 @@
 package com.example.servingwebcontent;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.servingwebcontent.database.songAiven;
 
 import java.util.ArrayList;
+import org.springframework.stereotype.Controller;
 
-
-
-
+//controller to song list
+@Controller
 public class SongListController {
     @GetMapping("/songlist")
+
     public String songlist(Model model){
 
         ArrayList<Song> songList = new ArrayList<Song>();
